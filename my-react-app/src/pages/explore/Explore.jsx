1800f459e6cf12e41e6089dba00b5daaca388065
krabhis <abhishekkr212402@gmail.com>
2025-06-09 23:53:44 +0530

    added code

---
+++ my-react-app/src/pages/explore/Explore.jsx
@@ -0,0 +1,45 @@
+import React , {useState , useEffect} from "react"
+import { useParams } from "react-router-dom"
+import Select from "react-select"
+
+import "./stytle.css"
+
+let filters={}
+
+const sortbyData = [
+    { value: "popularity.desc", label: "Popularity Descending" },
+    { value: "popularity.asc", label: "Popularity Ascending" },
+    { value: "vote_average.desc", label: "Rating Descending" },
+    { value: "vote_average.asc", label: "Rating Ascending" },
+    {
+        value: "primary_release_date.desc",
+        label: "Release Date Descending",
+    },
+    { value: "primary_release_date.asc", label: "Release Date Ascending" },
+    { value: "original_title.asc", label: "Title (A-Z)" },
+];
+
+
+
+const Explore=()=>{
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [genre, setGenre] = useState(null);
+    const [sortby, setSortby] = useState(null);
+    const { mediaType } = useParams();
+
+
+    
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/discover/${mediaType}`, filters).then((res) => {
+            setData(res);
+            setPageNum((prev) => prev + 1);
+            setLoading(false);
+        });
+    };
+
+    
+
+}
