2857f24549a27e4ea288c4f3379ac9082d429f44
krabhis <abhishekkr212402@gmail.com>
2025-06-19 00:24:13 +0530

    added components in pages

--- my-react-app/src/pages/explore/Explore.jsx
+++ my-react-app/src/pages/explore/Explore.jsx
@@ -1,10 +1,17 @@
-import React , {useState , useEffect} from "react"
-import { useParams } from "react-router-dom"
-import Select from "react-select"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Select from "react-select";
 
-import "./stytle.css"
+import "./style.scss";
 
-let filters={}
+import useFetch from "../../hooks/useFetch";
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
+
+let filters = {};  
 
 const sortbyData = [
     { value: "popularity.desc", label: "Popularity Descending" },
@@ -19,9 +26,7 @@ const sortbyData = [
     { value: "original_title.asc", label: "Title (A-Z)" },
 ];
 
-
-
-const Explore=()=>{
+const Explore = () => {
     const [data, setData] = useState(null);
     const [pageNum, setPageNum] = useState(1);
     const [loading, setLoading] = useState(false);
@@ -29,8 +34,8 @@ const Explore=()=>{
     const [sortby, setSortby] = useState(null);
     const { mediaType } = useParams();
 
+    const { data: genresData } = useFetch(`/genre/${mediaType}/list`);
 
-    
     const fetchInitialData = () => {
         setLoading(true);
         fetchDataFromApi(`/discover/${mediaType}`, filters).then((res) => {
@@ -40,6 +45,124 @@ const Explore=()=>{
         });
     };
 
-    
+    const fetchNextPageData = () => {
+        fetchDataFromApi(
+            `/discover/${mediaType}?page=${pageNum}`,
+            filters
+        ).then((res) => {
+            if (data?.results) {
+                setData({
+                    ...data,
+                    results: [...data?.results, ...res.results],
+                });
+            } else {
+                setData(res);
+            }
+            setPageNum((prev) => prev + 1);
+        });
+    };
 
-}
+    useEffect(() => {
+        filters = {};
+        setData(null);
+        setPageNum(1);
+        setSortby(null);
+        setGenre(null);
+        fetchInitialData();
+    }, [mediaType]);
+
+    const onChange = (selectedItems, action) => {
+        if (action.name === "sortby") {
+            setSortby(selectedItems);
+            if (action.action !== "clear") {
+                filters.sort_by = selectedItems.value;
+            } else {
+                delete filters.sort_by;
+            }
+        }
+
+        if (action.name === "genres") {
+            setGenre(selectedItems);
+            if (action.action !== "clear") {
+                let genreId = selectedItems.map((g) => g.id);
+                genreId = JSON.stringify(genreId).slice(1, -1);
+                filters.with_genres = genreId;
+            } else {
+                delete filters.with_genres;
+            }
+        }
+
+        setPageNum(1);
+        fetchInitialData();
+    };
+
+    return (
+        <div className="explorePage">
+            <ContentWrapper>
+                <div className="pageHeader">
+                    <div className="pageTitle">
+                        {mediaType === "tv"
+                            ? "Explore TV Shows"
+                            : "Explore Movies"}
+                    </div>
+                    <div className="filters">
+                        <Select
+                            isMulti
+                            name="genres"
+                            value={genre}
+                            closeMenuOnSelect={false}
+                            options={genresData?.genres}
+                            getOptionLabel={(option) => option.name}
+                            getOptionValue={(option) => option.id}
+                            onChange={onChange}
+                            placeholder="Select genres"
+                            className="react-select-container genresDD"
+                            classNamePrefix="react-select"
+                        />
+                        <Select
+                            name="sortby"
+                            value={sortby}
+                            options={sortbyData}
+                            onChange={onChange}
+                            isClearable={true}
+                            placeholder="Sort by"
+                            className="react-select-container sortbyDD"
+                            classNamePrefix="react-select"
+                        />
+                    </div>
+                </div>
+                {loading && <Spinner initial={true} />}
+                {!loading && (
+                    <>
+                        {data?.results?.length > 0 ? (
+                            <InfiniteScroll
+                                className="content"
+                                dataLength={data?.results?.length || []}
+                                next={fetchNextPageData}
+                                hasMore={pageNum <= data?.total_pages}
+                                loader={<Spinner />}
+                            >
+                                {data?.results?.map((item, index) => {
+                                    if (item.media_type === "person") return;
+                                    return (
+                                        <MovieCard
+                                            key={index}
+                                            data={item}
+                                            mediaType={mediaType}
+                                        />
+                                    );
+                                })}
+                            </InfiniteScroll>
+                        ) : (
+                            <span className="resultNotFound">
+                                Sorry, Results not found!
+                            </span>
+                        )}
+                    </>
+                )}
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default Explore;
